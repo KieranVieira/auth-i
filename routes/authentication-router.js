321d31c9d947e988130a336c7436860c9e70b48e
KieranVieira <kieranvieira@live.com>
2019-02-25 16:04:49 -0500

    Added login method

--- routes/authentication-router.js
+++ routes/authentication-router.js
@@ -1,8 +1,55 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const db = require('../database/dbConfig');
 
 const router = express.Router();
 
-router.get('/test', (req, res) => {
+router.post('/register', (req, res) => {
+    try {
+        let user = req.body;
+        user.password = bcrypt.hashSync(user.password, 12);
+    
+        db('users')
+            .insert(user)
+            .then(id => {
+                res.status(201).json(id)
+            })
+            .catch(error => {
+                res.status(400).json({
+                    message: "Bad request, username is taken or required fields arent filled",
+                    error
+                })
+            })
+    } catch (error) {
+        res.status(500).json({
+            message: "Server could not create user",
+            error
+        })
+    }
+});
+
+router.post('/login', (req, res) => {
+    let {username, password} = req.body;
+    db('users')
+        .where({username})
+        .first()
+        .then(user => {
+            if(user && bcrypt.compareSync(password, user.password)){
+                res.status(200).json({ message: `Welcome ${user.username}` })
+            }else{
+                res.status(401).json({ message: 'Invalid Credentials' })
+            }
+        })
+        .catch(error => {
+            res.status(500).json({
+                message: "Server could not login",
+                error
+            })
+        })
+
+});
+
+router.get('/users', (req, res) => {
     res.send('route works')
 });
 
