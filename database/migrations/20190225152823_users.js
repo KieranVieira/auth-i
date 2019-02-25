519e6c8c457d3f27d2c10774fc471ddbfb344b19
KieranVieira <kieranvieira@live.com>
2019-02-25 16:12:51 -0500

    Adjusted knex table (MVP Complete)

--- database/migrations/20190225152823_users.js
+++ database/migrations/20190225152823_users.js
@@ -3,7 +3,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', tbl => {
       tbl.increments();
       tbl.string('username').notNullable().unique();
-      tbl.string('password').notNullable().unique();
+      tbl.string('password').notNullable()
       tbl.timestamps(true,true);
   });
 };
