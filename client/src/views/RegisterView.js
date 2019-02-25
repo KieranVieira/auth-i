a19546ebc8e02465d6f35066152dc46fecf1bc79
KieranVieira <kieranvieira@live.com>
2019-02-25 18:06:53 -0500

    Added register form to client

--- client/src/views/RegisterView.js
+++ client/src/views/RegisterView.js
@@ -1,11 +1,47 @@
 import React from 'react';
+import axios from 'axios';
+
+import RegisterForm from '../components/RegisterForm';
 
 class RegisterView extends React.Component{
+    state={
+        registerInfo: {
+            username: '',
+            password: ''
+        }
+    }
+
+    handleRegisterChange = e => {
+        this.setState({
+            registerInfo: {
+                [e.target.name]: e.target.value
+            }
+        })
+    }
+
+    handleRegister = e => {
+        e.preventDefault()
+        axios.post('https://localhost:5000/api/register', this.state.loginInfo)
+            .then(res => {
+                this.setState({
+                    loggedIn: true,
+                    registerInfo: {
+                        username: '',
+                        password: ''
+                    }
+                })
+                this.props.history.push('/')
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+
     render(){
         return(
-            <h1>Register Component</h1>
+            <RegisterForm handleRegisterChange={this.handleRegisterChange} handleRegister={this.handleRegister}/>
         )
     }
 }
 
-export default RegisterView;
+export default RegisterView
